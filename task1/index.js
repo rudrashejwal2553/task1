5cd1f880ce1d327533cf867a3323c6c4c496a877
rudrashejwal2553 <126329714+rudrashejwal2553@users.noreply.github.com>
2024-07-11 13:38:15 +0530

    Add files via upload

---
+++ task1/index.js
@@ -0,0 +1,45 @@
+function validation (){
+    if(document.Formfill.Username.value==""){
+        document.getElementById("result").innerHTML="Enter Username *";
+        return false;
+    }
+    else if(document.Formfill.Username.value.length<=6){
+        document.getElementById("result").innerHTML="Atleast six letters*";
+        return false;
+
+}
+
+else if(document.Formfill.Email.value== ""){
+    document.getElementById("result").innerHTML="Enter your Email*";
+    return false;
+
+}
+else if(document.Formfill.Password.value== ""){
+    document.getElementById("result").innerHTML="Enter your Password*";
+    return false;
+}
+else if(document.Formfill.Password.value.length<8){
+    document.getElementById("result").innerHTML="Password must be 8-digits";
+    return false;
+}
+
+
+else if(document.Formfill.CPassword.value== ""){
+    document.getElementById("result").innerHTML="Enter Confirm  Password";
+    return false;
+}
+
+else if(document.Formfill.CPassword.value !== document.Formfill.Password.value ){
+    document.getElementById("result").innerHTML=" Password does'n matched";
+    return false;
+}
+
+else if(document.Formfill.Password.value !== document.Formfill.CPassword.value ){
+ popup.classList.add(".open-slide")
+    return false;
+}
+}
+var popup= document.getElementById('popup');
+function closeSlide(){
+    popup.classList.remove("open-slide")
+}
